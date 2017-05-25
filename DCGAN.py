9ad186e01d38ca28bcd133caa1aa30b28f60da3b
windChimeRan <1012869439@qq.com>
2017-05-25 16:40:51 +0800

    better vis

--- DCGAN.py
+++ DCGAN.py
@@ -22,6 +22,18 @@ def get_noise(batch_size):
 
 
 
+# def batch_norm(input_layer):
+#
+#     dimension = input_layer.get_shape().as_list()[-1]
+#     mean, variance = tf.nn.moments(input_layer, axes=[0, 1, 2])
+#     beta = tf.get_variable('beta', dimension, tf.float32,
+#                                initializer=tf.constant_initializer(0.0, tf.float32))
+#     gamma = tf.get_variable('gamma', dimension, tf.float32,
+#                                 initializer=tf.constant_initializer(1.0, tf.float32))
+#     bn_layer = tf.nn.batch_normalization(input_layer, mean, variance, beta, gamma, BN_EPSILON = 0.001)
+#
+#     return bn_layer
+
 
 
 
@@ -33,18 +45,6 @@ def vanilla_gan(X,Z):
     dim_W3 = 64
     dim_channel = 1
 
-    # def batch_norm(input_layer):
-    #
-    #     dimension = input_layer.get_shape().as_list()[-1]
-    #     mean, variance = tf.nn.moments(input_layer, axes=[0, 1, 2])
-    #     beta = tf.get_variable('beta', dimension, tf.float32,
-    #                                initializer=tf.constant_initializer(0.0, tf.float32))
-    #     gamma = tf.get_variable('gamma', dimension, tf.float32,
-    #                                 initializer=tf.constant_initializer(1.0, tf.float32))
-    #     bn_layer = tf.nn.batch_normalization(input_layer, mean, variance, beta, gamma, BN_EPSILON = 0.001)
-    #
-    #     return bn_layer
-
 
     batch_norm = partial(tf.contrib.layers.batch_norm, decay=0.9, epsilon=1e-5, scale=True, is_training=True)
     create_variables = partial(tf.get_variable, initializer=tf.contrib.layers.xavier_initializer())
@@ -52,6 +52,7 @@ def vanilla_gan(X,Z):
 
     def generator(Z, reuse=False):
         with tf.variable_scope('generator'):
+
 
             gen_W1 = create_variables(name='gen_W1',shape = [128,256])
             gen_W2 = create_variables(name='gen_W2', shape= [256, dim_W2 * 7 * 7])
@@ -67,7 +68,7 @@ def vanilla_gan(X,Z):
             h3 = tf.nn.conv2d_transpose(h2, gen_W3, output_shape=output_shape_l3, strides=[1, 2, 2, 1])
             h3 = tf.nn.elu(batch_norm(h3))
 
-            output_shape_l4 = [batch_size, 28, 28, dim_channel]
+            output_shape_l4 = [tf.shape(Z)[0], 28, 28, dim_channel]
             h4 = tf.nn.conv2d_transpose(h3, gen_W4, output_shape=output_shape_l4, strides=[1, 2, 2, 1])
             # h4 = tf.reshape(h4,[-1,28*28])
 
@@ -83,7 +84,8 @@ def vanilla_gan(X,Z):
             discrim_W1 = create_variables(name='discrim_W1', shape=[5, 5, dim_channel, dim_W3])
             discrim_W2 = create_variables(name='discrim_W2', shape=[5, 5, dim_W3 , dim_W2])
             discrim_W3 = create_variables(name='discrim_W3', shape=[5, 5, dim_W2 , dim_W1])
-            discrim_W5 = create_variables(name='discrim_W5', shape=[4096, 1])
+            discrim_W4 = create_variables(name='discrim_W4', shape=[4096, 256])
+            discrim_W5 = create_variables(name='discrim_W5', shape=[256, 1])
 
 
 
@@ -103,9 +105,10 @@ def vanilla_gan(X,Z):
 
             h2 = tf.reshape(h2,[batch_size,-1])
 
-            h3 = tf.matmul(h2, discrim_W5)
+            h3 = lrelu((tf.matmul(h2, discrim_W4)))
+            h4 = tf.matmul(h3, discrim_W5)
 
-            return h3
+            return h4
 
 
 
@@ -159,21 +162,23 @@ def main():
             _, loss_val_D = sess.run([train_D, loss_D], feed_dict={X: batch_xs, Z: noise})
             _, loss_val_G = sess.run([train_G, loss_G], feed_dict={Z: noise})
 
+
         print 'Epoch:', '%04d' % (epoch + 1), \
               'D loss: {:.4}'.format(loss_val_D), \
               'G loss: {:.4}'.format(loss_val_G)
 
 
-        sample_size = 10
-        noise = get_noise(batch_size)
+        sample_size = 100
+        noise = get_noise(sample_size)
 
         samples = sess.run(G, feed_dict={Z: noise})
 
-        fig, ax = plt.subplots(1, sample_size, figsize=(sample_size, 1))
+        fig, ax = plt.subplots(9, 9, figsize=(9, 9))
 
-        for i in range(sample_size):
-            ax[i].set_axis_off()
-            ax[i].imshow(np.reshape(samples[i], (28, 28)))
+        for i in range(9):
+            for j in range(9):
+                ax[i][j].set_axis_off()
+                ax[i][j].imshow(np.reshape(samples[i*9+j], (28, 28)))
 
         plt.savefig('logs/{}.png'.format(str(epoch).zfill(3)), bbox_inches='tight')
         plt.close(fig)
